1ffcc546599d5e30e5f14ff52495dfb943c641f2
ErazerBrecht <brecht.carlier@student.ap.be>
2020-08-15 14:57:29 +0200

    Add POC to VCS

---
+++ CryptoBE/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using IdentityModel;
+using IdentityModel.Jwk;
+
+namespace CryptoBE
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Crypto!");
+
+            Console.WriteLine("Public key (JWK)?");
+            var jwkString = Console.ReadLine();
+            var jwk = new JsonWebKey(jwkString);
+
+            Console.WriteLine("");
+            Console.WriteLine("Signature (base64)?");
+            var signBase64 = Console.ReadLine();
+            var sign = Convert.FromBase64String(signBase64 ?? string.Empty);
+
+            Console.WriteLine("");
+            Console.WriteLine("Payload?");
+            var payload = Console.ReadLine();
+            var encodedPayload = Encoding.UTF8.GetBytes(payload ?? string.Empty);
+
+            var rsa = GetPublicRsaKeyFromJwk(jwk);
+            var result = rsa.VerifyData(encodedPayload, sign, HashAlgorithmName.SHA256, RSASignaturePadding.Pss);
+
+            Console.WriteLine("");
+            if (result)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("The provided signature is VALID");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("The provided signature is INVALID");
+            }
+
+            Console.ReadLine();
+        }
+
+        private static RSA GetPublicRsaKeyFromJwk(JsonWebKey jwk)
+        {
+            var e = Base64Url.Decode(jwk.E);
+            var n = Base64Url.Decode(jwk.N);
+
+            var rsaKey = RSA.Create();
+            var keyParams = new RSAParameters
+            {
+                Exponent = e,
+                Modulus = n,
+            };
+
+            rsaKey.ImportParameters(keyParams);
+            return rsaKey;
+        }
+    }
+}
